0be02fcadd7f3886c0b4b3a6bbbe3d1e36ca1e73
Rodrigo Luque <rodrigoluquepuig@gmail.com>
2021-09-14 23:54:13 -0300

    Commit Inicial

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,7 +7,8 @@ namespace Roleplay
     {
         static void Main(string[] args)
         {
-            Elemento baston1 = new Elemento("Baston de Fuego");
+            Elemento baston1 = new Elemento("Baston de Fuego", 70, 0, "magico");
+            Elemento tunica1 = new Elemento("Tunica de Mago", 0, 30, "magico");
         }
     }
 }
