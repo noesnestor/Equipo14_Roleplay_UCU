2a8b8e49a53ca8c0316175e1886f79c6c45a4447
Rodrigo Luque <rodrigoluquepuig@gmail.com>
2021-09-15 10:59:58 -0300

    Modificado Program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,8 +7,8 @@ namespace Roleplay
     {
         static void Main(string[] args)
         {
-            Elemento baston1 = new Elemento("Baston de Fuego", 70, 0, "magico");
-            Elemento tunica1 = new Elemento("Tunica de Mago", 0, 30, "magico");
+            Elemento baston1 = new Elemento("Baston de Fuego", 70, 0, "Mago");
+            Elemento tunica1 = new Elemento("Tunica de Mago", 0, 30, "Mago");
         }
     }
 }
