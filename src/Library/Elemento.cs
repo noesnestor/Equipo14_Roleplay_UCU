75db8736d49a8baef3b812620be956520e04016d
Rodrigo Luque <rodrigoluquepuig@gmail.com>
2021-09-14 23:54:37 -0300

    Creada clase Elemento

---
+++ src/Library/Elemento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Roleplay
+{
+    public class Elemento
+    {
+        public string Nombre { get; }
+        public int Ataque { get; }
+        public int Defensa { get; }
+        public string Tipo { get; }
+
+        public Elemento(string nombre, int ataque, int defensa, string tipo)
+        {
+            this.Nombre = nombre;
+            this.Ataque = ataque;
+            this.Defensa = defensa;
+            this.Tipo = tipo;
+        }
+    }
+}
