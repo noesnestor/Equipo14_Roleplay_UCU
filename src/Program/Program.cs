4a7562902c8c94a57d01be052cdd5a04fd3684ab
Néstor Martínez <nestor.marvi@gmail.com>
2021-09-14 22:13:35 -0300

    Agregado la carpeta src con subcarpetas base

---
+++ src/Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Personaje_Juego
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
