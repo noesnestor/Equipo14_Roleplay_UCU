332ffdc585b34a88a3e777472ee9cdfd25d714ac
Rodrigo Luque <rodrigoluquepuig@gmail.com>
2021-09-16 23:52:02 -0300

    Agregado comentarios a la clase

--- src/Library/Personaje.cs
+++ src/Library/Personaje.cs
@@ -5,8 +5,6 @@ namespace Roleplay
 {
     public class Personaje
     {
-        private ArrayList inventario = new ArrayList();
-
         public Personaje(string nombre, string raza, int salud)
         {
             this.Nombre = nombre;
@@ -15,15 +13,24 @@ namespace Roleplay
             this.VidaMax = salud;
         }
 
+        // Variables
+        private ArrayList inventario = new ArrayList();
         private int ataque;
         private int defensa;
 
-
+        // Propiedades
         public string Nombre {get; set;}
         public int Salud {get; private set;}
         public string Raza {get;}
         public int VidaMax {get;}
 
+        /*
+        Para obtener tanto el ataque como la defensa del personaje
+        se decidio crear respectivamente sus propiedades en el personaje, ya que es el encargado
+        de conocer el inventario, donde en el get de cada una
+        se recorre todos los elementos que tiene creando asi el subtotal de valores necesarios
+        para calcular tanto la defensa como el ataque total.
+        */
         public int Ataque 
         {
             get
@@ -35,6 +42,7 @@ namespace Roleplay
                 return this.ataque;
             } 
         }
+
         public int Defensa 
         {
             get
@@ -46,7 +54,6 @@ namespace Roleplay
                 return this.defensa;
             }
         }
-
         
         public void AgregarElemento(Elemento elemento)
         {
@@ -73,6 +80,12 @@ namespace Roleplay
             }
         }
 
+        /*
+        El ataque se realiza a partir de un metodo Atacar,
+        se le pasa por parametro otro personaje. Esto se decidio, ya que al atacar
+        es necesario obtener los valores tanto de ataque como de defensa del objetivo y esta clase
+        es la encargada de conocer estos valores.
+        */
         public void Atacar(Personaje personaje)
         {
             int dmg = this.Ataque - personaje.Defensa;
@@ -95,6 +108,11 @@ namespace Roleplay
             }
         }
 
+        /*
+        Dado que es el jugador quien tiene acceso a los valores de su salud,
+        nos parecio que deberia tener la capacidad de curarse a si mismo, de esta forma
+        poder cambiar sus atributos propios.
+        */
         public void Curar(int vidaACurar)
         {
             if (this.Salud + vidaACurar > this.VidaMax)
@@ -107,7 +125,6 @@ namespace Roleplay
                 this.Salud += vidaACurar;
                 Console.WriteLine($"Se ha curado a {this.Nombre}, ahora tiene {this.Salud} de puntos de vida.");
             }
-            
         }
     }
 }
